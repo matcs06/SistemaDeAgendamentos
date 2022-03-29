6419b8834b9e040169fd06700de8341121500ea3
Mateus Costa Silva <matcs06@gmail.com>
2022-03-28 23:21:41 -0300

    fixing popup

--- src/pages/admin/openClient/index.tsx
+++ src/pages/admin/openClient/index.tsx
@@ -4,7 +4,7 @@ export default function OpenClient(){
    const [ isFirstTime, setIsFirstTime] = useState(true)
 
       useEffect(()=>{
-         window.open("/client/startPage/",'mywin', "width=500, height=900")
+         window.open("/client/startPage/",'_blank', "width=500, height=900")
          window.location.pathname = "/admin/servicesList"
       },[isFirstTime])
 
